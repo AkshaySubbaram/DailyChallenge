79b2b6b5b6dd2562b029721d7d70338375a448ca
AkshaySubbaram <subbaramakshay@gmail.com>
2025-09-04 19:53:45 +0530

    sep 9

--- src/Practice.java
+++ src/Practice.java
@@ -38,8 +38,32 @@ public class Practice {
         return waterTrapped;
     }
 
+//    ToDo: maximum product of element in given sub arr
+//    only adjecent elements no random pair
+
+    public static int maxProduct(int[] nums) {
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            int tempMax = Math.max(curr, Math.max(curr * maxSoFar, curr * minSoFar));
+
+            minSoFar = Math.min(curr, Math.min(curr * maxSoFar, curr * minSoFar));
+            maxSoFar = tempMax;
+            result = Math.max(result, maxSoFar);
+        }
+        return result;
+    }
+
     public static void main(String[] args){
         int[] heights = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println("Trapped water: " + trap(heights));
+
+        int[] nums = {2, 3, -2, 4};
+        System.out.println("Max Product: " + maxProduct(nums));
     }
+
+
 }
