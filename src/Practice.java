b0be760b3186e86e6dae564011ba4677fdb74282
AkshaySubbaram <subbaramakshay@gmail.com>
2025-09-04 18:45:58 +0530

    sep 9

--- src/Practice.java
+++ src/Practice.java
@@ -4,6 +4,15 @@ public class Practice {
 //    compute how much water can be trapped after raining
 //    Example: {0,1,0,2,1,0,1,3,2,1,2,1} -> 6
 
+//    in this case
+//    first left = 0 right = 1
+//        0<1 so l++
+//        1>=1 so r--
+//        1<2 so l++
+//        0<2 so l++
+//        1<2 so l++
+//        3>2 so r-- ToDo: each time we should update the position
+
     public static int trap(int[] height) {
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
